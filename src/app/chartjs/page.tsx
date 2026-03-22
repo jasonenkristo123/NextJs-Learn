9d2b3d4d20de73048d1b9680ede29d2458245404
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-22 21:58:04 +0700

    learn chartjs

---
+++ src/app/chartjs/page.tsx
@@ -0,0 +1,159 @@
+"use client"
+import 'chart.js/auto';
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import { sourceData } from "./sourceData";
+
+export default function ChartJs() {
+  return (
+    <section>
+      <h1 className="text-4xl text-black">Chart JS Practice</h1>
+
+      <div className="w-full grid grid-cols-2 grid-rows-1 p-10 gap-10">
+        {/* chart1 */}
+        <div className="bg-slate-100 shadow-2xl rounded-xl col-span-2 w-full h-100 p-10">
+          <div>Chart 1</div>
+          <Line
+            data={{
+              labels: sourceData.map((data) => data.label),
+              datasets: [
+                {
+                  label: "Revenue",
+                  data: sourceData.map((data) => data.revenue),
+                  backgroundColor: "rgba(255, 99, 132, 0.5)",
+                  borderColor: "rgba(255, 99, 132, 1)",
+                },
+                {
+                  label: "Cost",
+                  data: sourceData.map((data) => data.cost),
+                  backgroundColor: "rgba(54, 162, 235, 0.5)",
+                  borderColor: "rgba(54, 162, 235, 1)",
+                },
+              ],
+            }}
+
+            options={{
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                            display: true,
+                            text: 'Monthly Revenue and Cost',
+                            font: {
+                                size: 24
+                            }
+                    }
+                },
+                animation: {
+                    duration: 1000,
+                    easing: 'easeInOutQuart',
+                    loop: false
+                }
+            }}
+          />
+        </div>
+
+        <div className="bg-slate-300 rounded-xl w-full h-100 col-span-1 p-10">
+          <div>Chart 2</div>
+
+            <Bar
+                data={{
+                    labels: sourceData.map((data) => data.label),
+                    datasets: [
+                        {
+                            label: "Revenue",
+                            data: sourceData.map((data) => data.revenue),
+                            backgroundColor: "rgba(255, 99, 132, 0.5)",
+                            borderColor: "rgba(255, 99, 132, 1)",
+                        },
+                        {
+                            label: "Cost",
+                            data: sourceData.map((data) => data.cost),
+                            backgroundColor: "rgba(54, 162, 235, 0.5)",
+                            borderColor: "rgba(54, 162, 235, 1)",
+                        }
+                    ]
+                }} 
+                options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        title: {
+                            text: "Revenue and Cost Sources",
+                            display: true,
+                            font: {
+                                size: 24
+                            }
+                        }
+                    },
+                    animation: {
+                        duration: 1000,
+                        easing: 'easeInOutQuart',
+                        
+                    }
+                }}
+             />
+        </div>
+
+        <div className="bg-slate-300 rounded-xl w-full h-100 col-span-1 p-10">
+          <div>Chart 3</div>
+          <Doughnut
+            data={{
+                labels: sourceData.map((data) => data.label),
+                datasets: [
+                    {
+                        label: "Revenue",
+                        data: sourceData.map((data) => data.revenue),
+                        backgroundColor: [
+                            "rgba(255, 99, 132, 0.5)",
+                            "rgba(54, 162, 235, 0.5)",
+                            "rgba(255, 206, 86, 0.5)",
+                            "rgba(75, 192, 192, 0.5)",
+                            "rgba(153, 102, 255, 0.5)",
+                            "rgba(255, 159, 64, 0.5)",
+                            "rgba(255, 99, 132, 0.5)",
+                            "rgba(54, 162, 235, 0.5)",
+                            "rgba(255, 206, 86, 0.5)",
+                            "rgba(75, 192, 192, 0.5)",
+                            "rgba(153, 102, 255, 0.5)",
+                            "rgba(255, 159, 64, 0.5)"
+                        ],
+                        borderColor: [
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                            "rgba(153, 102, 255, 1)",
+                            "rgba(255, 159, 64, 1)",
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                            "rgba(153, 102, 255, 1)",
+                            "rgba(255, 159, 64, 1)"
+                        ],
+                    },
+                ]
+            }}
+            options={{
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Revenue n Cost",
+                        font: {
+                            size: 24
+                        }
+                    }
+                },
+                animation: {
+                    duration: 1000,
+                    easing: 'easeInOutQuart',
+                }
+            }}
+           />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/chartjs/sourceData.ts
@@ -0,0 +1,64 @@
+
+
+export const sourceData = [
+    {
+        "label": "Jan",
+        "revenue": 40000,
+        "cost": 20000
+    },
+    {
+        "label": "Feb",
+        "revenue": 45000,
+        "cost": 25000
+    },
+    {
+        "label": "Mar",
+        "revenue": 50000,
+        "cost": 30000
+    },
+    {
+        "label": "Apr",
+        "revenue": 55000,
+        "cost": 35000
+    },
+    {
+        "label": "May",
+        "revenue": 60000,
+        "cost": 40000
+    },
+    {
+        "label": "Jun",
+        "revenue": 65000,
+        "cost": 45000
+    },
+    {
+        "label": "Jul",
+        "revenue": 70000,
+        "cost": 50000
+    },
+    {
+        "label": "Aug",
+        "revenue": 75000,
+        "cost": 55000
+    },
+    {
+        "label": "Sep",
+        "revenue": 80000,
+        "cost": 60000
+    },
+    {
+        "label": "Oct",
+        "revenue": 85000,
+        "cost": 65000
+    },
+    {
+        "label": "Nov",
+        "revenue": 90000,
+        "cost": 70000
+    },
+    {
+        "label": "Dec",
+        "revenue": 95000,
+        "cost": 75000
+    }
+]
