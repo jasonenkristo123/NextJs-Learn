a60a0f40c2938e5a5af1c88d7ab23b45906a3158
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-23 19:13:01 +0700

    home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <section>
       <h1 className="text-4xl text-black">Home Page</h1>
+      <div>
+        <p>Hello Wwelcom</p>
+      </div>
     </section>
   );
 }
