1b78f1ffe8fda7f76a7ee42b681d6302f410dc64
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-24 16:10:45 +0700

    awkoawo

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,44 @@
 export default function Home() {
   return (
     <section>
-      
+        <div className="w-full bg-black h-screen flex items-center justify-center text-white">
+            <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div>
+                        <div>
+                          <div>
+                            <div>
+                              <div>
+                                <div>
+                                  <div>
+                                    <div>
+                                      <div>
+                                        <div>
+                                          <div>
+                                            <div>
+                                              
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
     </section>
   );
 }
