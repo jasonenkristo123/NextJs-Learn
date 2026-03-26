0d8b26f367f6e7e418781a7aab328c3d1d3452e5
jasonenkristo123 <jasonenkristo@gmail.com>
2026-03-26 23:24:36 +0700

    awkaw

--- src/app/chartjs/page.tsx
+++ src/app/chartjs/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import 'chart.js/auto';
+"use client";
+import "chart.js/auto";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
 import { sourceData } from "./sourceData";
 
@@ -30,24 +30,23 @@ export default function ChartJs() {
                 },
               ],
             }}
-
             options={{
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    title: {
-                            display: true,
-                            text: 'Monthly Revenue and Cost',
-                            font: {
-                                size: 24
-                            }
-                    }
+              responsive: true,
+              maintainAspectRatio: false,
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Monthly Revenue and Cost",
+                  font: {
+                    size: 24,
+                  },
                 },
-                animation: {
-                    duration: 1000,
-                    easing: 'easeInOutQuart',
-                    loop: false
-                }
+              },
+              animation: {
+                duration: 1000,
+                easing: "easeInOutQuart",
+                loop: false,
+              },
             }}
           />
         </div>
@@ -55,103 +54,102 @@ export default function ChartJs() {
         <div className="bg-slate-300 rounded-xl w-full h-100 col-span-1 p-10">
           <div>Chart 2</div>
 
-            <Bar
-                data={{
-                    labels: sourceData.map((data) => data.label),
-                    datasets: [
-                        {
-                            label: "Revenue",
-                            data: sourceData.map((data) => data.revenue),
-                            backgroundColor: "rgba(255, 99, 132, 0.5)",
-                            borderColor: "rgba(255, 99, 132, 1)",
-                        },
-                        {
-                            label: "Cost",
-                            data: sourceData.map((data) => data.cost),
-                            backgroundColor: "rgba(54, 162, 235, 0.5)",
-                            borderColor: "rgba(54, 162, 235, 1)",
-                        }
-                    ]
-                }} 
-                options={{
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    plugins: {
-                        title: {
-                            text: "Revenue and Cost Sources",
-                            display: true,
-                            font: {
-                                size: 24
-                            }
-                        }
-                    },
-                    animation: {
-                        duration: 1000,
-                        easing: 'easeInOutQuart',
-                        
-                    }
-                }}
-             />
+          <Bar
+            data={{
+              labels: sourceData.map((data) => data.label),
+              datasets: [
+                {
+                  label: "Revenue",
+                  data: sourceData.map((data) => data.revenue),
+                  backgroundColor: "rgba(255, 99, 132, 0.5)",
+                  borderColor: "rgba(255, 99, 132, 1)",
+                },
+                {
+                  label: "Cost",
+                  data: sourceData.map((data) => data.cost),
+                  backgroundColor: "rgba(54, 162, 235, 0.5)",
+                  borderColor: "rgba(54, 162, 235, 1)",
+                },
+              ],
+            }}
+            options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              plugins: {
+                title: {
+                  text: "Revenue and Cost Sources",
+                  display: true,
+                  font: {
+                    size: 24,
+                  },
+                },
+              },
+              animation: {
+                duration: 1000,
+                easing: "easeInOutQuart",
+              },
+            }}
+          />
         </div>
 
         <div className="bg-slate-300 rounded-xl w-full h-100 col-span-1 p-10">
           <div>Chart 3</div>
           <Doughnut
             data={{
-                labels: sourceData.map((data) => data.label),
-                datasets: [
-                    {
-                        label: "Revenue",
-                        data: sourceData.map((data) => data.revenue),
-                        backgroundColor: [
-                            "rgba(255, 99, 132, 0.5)",
-                            "rgba(54, 162, 235, 0.5)",
-                            "rgba(255, 206, 86, 0.5)",
-                            "rgba(75, 192, 192, 0.5)",
-                            "rgba(153, 102, 255, 0.5)",
-                            "rgba(255, 159, 64, 0.5)",
-                            "rgba(255, 99, 132, 0.5)",
-                            "rgba(54, 162, 235, 0.5)",
-                            "rgba(255, 206, 86, 0.5)",
-                            "rgba(75, 192, 192, 0.5)",
-                            "rgba(153, 102, 255, 0.5)",
-                            "rgba(255, 159, 64, 0.5)"
-                        ],
-                        borderColor: [
-                            "rgba(255, 99, 132, 1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)",
-                            "rgba(255, 99, 132, 1)",
-                            "rgba(54, 162, 235, 1)",
-                            "rgba(255, 206, 86, 1)",
-                            "rgba(75, 192, 192, 1)",
-                            "rgba(153, 102, 255, 1)",
-                            "rgba(255, 159, 64, 1)"
-                        ],
-                    },
-                ]
+              labels: sourceData.map((data) => data.label),
+              datasets: [
+                {
+                  label: "Revenue",
+                  data: sourceData.map((data) => data.revenue),
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.5)",
+                    "rgba(54, 162, 235, 0.5)",
+                    "rgba(255, 206, 86, 0.5)",
+                    "rgba(75, 192, 192, 0.5)",
+                    "rgba(153, 102, 255, 0.5)",
+                    "rgba(255, 159, 64, 0.5)",
+                    "rgba(255, 99, 132, 0.5)",
+                    "rgba(54, 162, 235, 0.5)",
+                    "rgba(255, 206, 86, 0.5)",
+                    "rgba(75, 192, 192, 0.5)",
+                    "rgba(153, 102, 255, 0.5)",
+                    "rgba(255, 159, 64, 0.5)",
+                  ],
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                  ],
+                },
+              ],
             }}
             options={{
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    title: {
-                        display: true,
-                        text: "Revenue n Cost",
-                        font: {
-                            size: 24
-                        }
-                    }
+              responsive: true,
+              maintainAspectRatio: false,
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Revenue n Cost",
+                  font: {
+                    size: 24,
+                  },
                 },
-                animation: {
-                    duration: 1000,
-                    easing: 'easeInOutQuart',
-                }
+              },
+              animation: {
+                duration: 1000,
+                easing: "easeInOutQuart",
+              },
             }}
-           />
+          />
         </div>
       </div>
     </section>
