5d1e7dc7675ca3ec3a01594861281734a9e33275
jasonenkristo123 <jasonenkristo@gmail.com>
2026-04-01 22:47:58 +0700

    eoawkawe

---
+++ src/app/button/button.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+function button() {
+  return (
+    <div>button</div>
+
+    <div>
+        <div>
+            <div>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"></button>
+                    Click Me
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default button
