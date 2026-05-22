979bb274521254d48ad0704738a58fec7c468c8a
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-22 21:36:02 +0700

    awkoaw

--- src/app/home/Count.tsx
+++ src/app/home/Count.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React from 'react'
 
 function Count() {
-  const [count, setCount] = useState(0);
   return (
-    
-  );
+    <div>
+      <h1>
+        Malas
+      </h1>
+      <p>
+        pelis
+      </p>
+    </div>
+  )
 }
 
-export default Count;
+export default Count
