020c47a5e75e3b4acf4d2dc5e11238ea234d193f
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-23 21:07:35 +0700

    awek

--- src/app/home/Count.tsx
+++ src/app/home/Count.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 
 function Count() {
   return (
-    <div>
-      <h1>
-        Malas
-      </h1>
-      <p>
-        pelis
-      </p>
-    </div>
+    <section>
+      <input type="number" placeholder="Enter a number" />
+      <button>Count</button>
+      
+    </section>
   )
 }
 
