d5e53240cee292308641a879a3c6a061cd3377e6
jasonenkristo123 <jasonenkristo@gmail.com>
2026-05-24 21:23:59 +0700

    wakoaw

--- src/app/home/Count.tsx
+++ src/app/home/Count.tsx
@@ -5,6 +5,13 @@ function Count() {
     <section>
       <input type="number" placeholder="Enter a number" />
       <button>Count</button>
+      <p>
+        The count will be displayed here.
+      </p>
+      <input type="text" />
+      <h1>
+        The result will be displayed here.
+      </h1>
       
     </section>
   )
